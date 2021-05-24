e2a56be22ed126a8c5d046b4d25a96566ed2148f
Lane <lanericharddavis@gmail.com>
2021-05-24 08:49:23 -0600

    incomplete

--- Controllers/CommentsController.cs
+++ Controllers/CommentsController.cs
@@ -1,28 +1,95 @@
+using System;
+using System.Threading.Tasks;
+using blogger.Models;
 using blogger.Services;
+using CodeWorks.Auth0Provider;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace blogger.Controllers
 {
   [ApiController]
-  [Route("[controller]")]
+  [Route("api/[controller]")]
   public class CommentsController : ControllerBase
   {
     private readonly CommentsService _service;
-    public CommentsController(CommentsService service)
+    private readonly ProfilesService _profileService;
+    public CommentsController(CommentsService service, ProfilesService profilesService)
     {
       _service = service;
+      _profileService = profilesService;
     }
 
 
-    // [HttpPost("{id}/comments")]
-    //TODO create a new comment
-    //NOTE Need to be logged in
-    // [HttpPut("{id}")]
-    //TODO edit single comment
-    //NOTE Need to be authorized (can only modify your own)
-    // [HttpDelete("{id}")]
-    //TODO delete single comment
-    //NOTE Need to be authorized (can only modify your own)
+    [HttpPost]
+    [Authorize]
+    public async Task<ActionResult<Comment>> Create([FromBody] Comment newComment)
+    {
+      try
+      {
+        Profile userInfo = await HttpContext.GetUserInfoAsync<Profile>();
+        Profile fullProfile = _profileService.GetOrCreateAccountProfile(userInfo);
+        newComment.CreatorId = userInfo.Id;
+
+        Comment comment = _service.CreateComment(newComment);
+        comment.Creator = fullProfile;
+        return Ok(comment);
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Comment> GetOneComment(int id)
+    {
+      try
+      {
+        Comment found = _service.GetOneComment(id);
+        return Ok(found);
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpPut("{id}")]
+    [Authorize]
+    public ActionResult<Comment> EditComment(int id, [FromBody] Comment newComment)
+    {
+      try
+      {
+        Profile userInfo = await HttpContext.GetUserInfoAsync<Profile>();
+        Profile fullProfile = _profileService.GetOrCreateAccountProfile(userInfo);
+        newComment.CreatorId = userInfo.Id;
+        Comment comment = _service.CreateComment(newComment);
+        comment.Creator = fullProfile;
+        Comment found = _service.EditComment(id);
+        return Ok(found);
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpDelete("{id}")]
+    [Authorize]
+    public async Task<ActionResult<Comment>> DeleteComment(int id)
+    {
+      try
+      {
+        Profile userInfo = await HttpContext.GetUserInfoAsync<Profile>();
+        _service.DeleteComment(id, userInfo.Id);
+        return Ok("Comment Deleted");
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
 
   }
 }
--- Repositories/CommentsRepository.cs
+++ Repositories/CommentsRepository.cs
@@ -1,7 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using blogger.Models;
+using Dapper;
+
 namespace blogger.Repositories
 {
-    public class CommentsRepository
+  public class CommentsRepository
+  {
+    private readonly IDbConnection _db;
+    public CommentsRepository(IDbConnection db)
     {
-        
+      _db = db;
     }
+
+    internal Comment GetOneComment(int id)
+    {
+      string sql = @"
+      SELECT
+      c.*,
+      p.*
+      FROM comments c
+      JOIN profiles p ON c.creatorId = p.id
+      WHERE id = @id";
+      return _db.Query<Comment, Profile, Comment>(sql, (comment, profile) =>
+      {
+        comment.Creator = profile;
+        return comment;
+      }
+      , new { id }, splitOn: "id").FirstOrDefault();
+    }
+
+    internal Comment EditComment(int id)
+    {
+      string sql = @"
+      UPDATE comments
+      SET
+        body = 
+      ";
+    }
+
+    internal IEnumerable<Comment> GetAllBlogsComments(int id)
+    {
+      string sql = "SELECT * FROM comments WHERE blog = @id";
+      return _db.Query<Comment>(sql, new { id });
+    }
+
+    internal Comment CreateComment(Comment newComment)
+    {
+      string sql = @"
+      INSERT INTO comments
+      (creatorId, body, blog)
+      VALUE
+      (@CreatorId, @Body, @Blog);
+      SELECT LAST_INSERT_ID()";
+      newComment.Id = _db.ExecuteScalar<int>(sql, newComment);
+      return newComment;
+    }
+
+    internal bool DeleteComment(int id)
+    {
+      string sql = "DELETE FROM comments WHERE id = @id LIMIT 1";
+      int affectedRows = _db.Execute(sql, new { id });
+      return affectedRows == 1;
+    }
+
+  }
 }
